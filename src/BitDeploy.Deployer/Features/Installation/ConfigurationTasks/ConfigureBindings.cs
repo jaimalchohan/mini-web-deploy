7a17a99c54699923eb039a3053be01ab280398c5
David Whitney <david@davidwhitney.co.uk>
2013-12-15 12:29:33 +0000

    inversion of guard check

--- src/BitDeploy.Deployer/Features/Installation/ConfigurationTasks/ConfigureBindings.cs
+++ src/BitDeploy.Deployer/Features/Installation/ConfigurationTasks/ConfigureBindings.cs
@@ -12,17 +12,19 @@ namespace BitDeploy.Deployer.Features.Installation.ConfigurationTasks
 
         public override void ConfigureInstalledSite(Site site, InstallationConfiguration configuration)
         {
-            if (configuration.Bindings.Any())
+            if (!configuration.Bindings.Any())
             {
-                site.Bindings.Clear();
+                return;
+            }
 
-                foreach (var binding in configuration.Bindings)
-                {
-                    var b = site.Bindings.CreateElement();
-                    b.Protocol = binding.Protocol;
-                    b.BindingInformation = string.Format("{0}:{1}:{2}", binding.IPAddress, binding.Port, binding.Host);
-                    site.Bindings.Add(b);
-                }
+            site.Bindings.Clear();
+
+            foreach (var binding in configuration.Bindings)
+            {
+                var b = site.Bindings.CreateElement();
+                b.Protocol = binding.Protocol;
+                b.BindingInformation = string.Format("{0}:{1}:{2}", binding.IPAddress, binding.Port, binding.Host);
+                site.Bindings.Add(b);
             }
         }
     }
