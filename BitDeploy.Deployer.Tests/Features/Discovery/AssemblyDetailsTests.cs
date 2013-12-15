8fa7332ad5b5c26d6296a57d02d053016b4d04d1
David Whitney <david@davidwhitney.co.uk>
2013-12-15 13:55:21 +0000

    Adding test project and some obvious simple tests.

---
+++ BitDeploy.Deployer.Tests/Features/Discovery/AssemblyDetailsTests.cs
@@ -0,0 +1,18 @@
+﻿using BitDeploy.Deployer.Features.Discovery;
+using NUnit.Framework;
+
+namespace BitDeploy.Deployer.Tests.Features.Discovery
+{
+    [TestFixture]
+    public class AssemblyDetailsTests
+    {
+        [Test]
+        public void Ctor_StoresPathsProvided()
+        {
+            var instance = new AssemblyDetails("one", "two");
+
+            Assert.That(instance.Path, Is.EqualTo("one"));
+            Assert.That(instance.BinaryPath, Is.EqualTo("two"));
+        }
+    }
+}
---
+++ BitDeploy.Deployer.Tests/Features/Discovery/ConfiguredInstallationManifestTests.cs
@@ -0,0 +1,25 @@
+﻿using BitDeploy.Deployer.Features.Discovery;
+using BitDeploy.Deployer.Features.Installation;
+using BitDeploy.Deployer.Tests.Fakes;
+using NUnit.Framework;
+
+namespace BitDeploy.Deployer.Tests.Features.Discovery
+{
+    [TestFixture]
+    public class ConfiguredInstallationManifestTests
+    {
+        [Test]
+        public void Ctor_StoresProvidedArguments()
+        {
+            var config = new InstallationConfiguration("path");
+            var installer = new TestInstaller();
+            const string path = "path";
+
+            var dto = new ConfiguredInstallationManifest(config, installer, path);
+
+            Assert.That(dto.InstallationConfiguration, Is.EqualTo(config));
+            Assert.That(dto.SourceInstaller, Is.EqualTo(installer));
+            Assert.That(dto.Path, Is.EqualTo(path));
+        }
+    }
+}
