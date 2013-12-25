e6a3dc91757a086501785583a770f0a0135d7aaf
Jaimal Chohan <jaimalchohan@gmail.com>
2013-12-25 17:22:22 +0000

    Ensure assemblyinfo contains correct version

--- Rakefile.rb
+++ Rakefile.rb
@@ -13,8 +13,19 @@ nugets_restore :restore do |p|
   p.exe = '.nuget/NuGet.exe'
 end
 
+task :patch_assembly_info => [:versioning] do
+	FileList.new("**/*.csproj").each{ |p|
+		project = Pathname.new p
+		file_name = File.join(project.dirname.to_s, 'Properties/AssemblyInfo.cs')
+		text = File.read(file_name)
+	  	text  = text.gsub(/AssemblyVersion.+$/, "AssemblyVersion(\"#{ENV["FORMAL_VERSION"]}\")]")
+		text  = text.gsub(/AssemblyFileVersion.+$/, "AssemblyFileVersion(\"#{ENV["FORMAL_VERSION"]}\")]")
+		File.open(file_name, "w") {|file| file.puts text}
+	}
+end
+
 desc 'Perform full build'
-build :build => [:versioning, :restore] do |b|
+build :build => [:versioning, :restore, :patch_assembly_info] do |b|
   b.sln = 'MiniWebDeploy.sln'
   b.prop 'Configuration', 'Release'
 end
@@ -28,8 +39,8 @@ nugets_pack :create_nugets => [package_dir, :versioning, :build] do |p|
   p.configuration = 'Release'
   p.exe     = '.nuget/NuGet.exe'
   p.with_metadata do |m|
-    m.description = 'A cool nuget'
-    m.authors = 'Henrik'
+    m.description = 'An easy to use web deployment tool'
+    m.authors = 'Jaimal Chohan'
     m.version = ENV['NUGET_VERSION']
   end
   p.with_package do |p|
