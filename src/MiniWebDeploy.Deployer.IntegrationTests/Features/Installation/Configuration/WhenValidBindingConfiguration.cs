f0446c87579ffc6428ed305033fac622837eaad0
Jaimal Chohan <jaimal.chohan@just-eat.com>
2014-01-24 11:46:44 +0000

    Added further tests around ConfigureBindings

--- src/MiniWebDeploy.Deployer.IntegrationTests/Features/Installation/Configuration/WhenValidBindingConfiguration.cs
+++ src/MiniWebDeploy.Deployer.IntegrationTests/Features/Installation/Configuration/WhenValidBindingConfiguration.cs
@@ -1,24 +1,25 @@
-﻿using Microsoft.Web.Administration;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Web.Administration;
 using MiniWebDeploy.Deployer.Features.Installation;
 using MiniWebDeploy.Deployer.Features.Installation.Configuration;
 using MiniWebDeploy.Deployer.Features.Installation.Installation;
 using MiniWebDeploy.Deployer.Infrastructure.IIS7Plus;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace MiniWebDeploy.Deployer.IntegrationTests.Features.Installation.Configuration
 {
     [TestFixture]
-    public class WhenNoBindingConfiguration : SiteTestBase
+    public class WhenValidBindingConfiguration : SiteTestBase
     {
         private Site _site;
         private IEnumerable<Microsoft.Web.Administration.Binding> _existingBindings;
 
         protected override void Given(InstallationConfiguration installationConfiguration)
         {
-
+            installationConfiguration
+                .AndHttpBinding("myhost1")
+                .AndHttpBinding("myhost2", "8.8.8.8");
         }
 
         protected override void When(ServerManagerWrapper manager)
@@ -26,22 +27,26 @@ namespace MiniWebDeploy.Deployer.IntegrationTests.Features.Installation.Configur
             var createSite = new CreateSite(manager);
             _site = createSite.Install(InstallationConfiguration);
 
-           _existingBindings = _site.Bindings.Select(x => x);
-
             var bindings = new ConfigureBindings(manager);
             bindings.ConfigureInstalledSite(_site, InstallationConfiguration);
         }
 
         [Test]
-        public void SameNumberOfBindings()
+        public void TwoBindingsAreCreated()
         {
-            Assert.AreEqual(_existingBindings.Count(), _site.Bindings.ToArray().Length);
+            Assert.AreEqual(2, _site.Bindings.ToArray().Length);
         }
 
         [Test]
-        public void AllBindingsAreTheSame()
+        public void MyHost1BindingHasBeenCreated()
         {
-            Assert.That(_existingBindings.All(x => _site.Bindings.Contains(x)));
+            Assert.That(_site.Bindings.Any(x => x.Host == "myhost1"));
+        }
+
+        [Test]
+        public void MyHost2BindingHasBeenCreated()
+        {
+            Assert.That(_site.Bindings.Any(x => x.Host == "myhost2" && x.EndPoint.Address.ToString() == "8.8.8.8"));
         }
     }
 }
