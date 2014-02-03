097987a19b2a01797913bcf6638db350c025410c
Jaimal Chohan <jaimalchohan@gmail.com>
2014-02-03 23:49:54 +0000

    Added ConfigurationTaskList tests

---
+++ src/MiniWebDeploy.Deployer.Tests/Features/Installation/ConfigurationTaskListTests.cs
@@ -0,0 +1,26 @@
+﻿using MiniWebDeploy.Deployer.Features.Installation;
+using MiniWebDeploy.Deployer.Features.Installation.Configuration;
+using MiniWebDeploy.Deployer.Features.Installation.Installation;
+using Moq;
+using NUnit.Framework;
+
+namespace MiniWebDeploy.Deployer.Tests.Features.Installation
+{
+    [TestFixture]
+    public class ConfigurationTaskListTests
+    {
+        [Test]
+        public void ConfigureInstalledSiteIsCalledForAllTasks()
+        {
+            var confTask = new Mock<IConfigurationTask>();
+
+            var taskList = new ConfigurationTaskList();
+            taskList.Add(confTask.Object);
+            taskList.Add(confTask.Object);
+
+            taskList.Configure(null, new InstallationConfiguration(null, null));
+
+            confTask.Verify(x => x.ConfigureInstalledSite(null, It.IsAny<InstallationConfiguration>()), Times.Exactly(2));
+        }
+    }
+}
