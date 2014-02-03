255d35e53f297d55800e89d48e324068ad236783
Jaimal Chohan <jaimalchohan@gmail.com>
2014-02-03 23:52:25 +0000

    Added PreInstallationTaskList tests

---
+++ src/MiniWebDeploy.Deployer.Tests/Features/Installation/PreInstallationTaskListTests.cs
@@ -0,0 +1,25 @@
+﻿using MiniWebDeploy.Deployer.Features.Installation;
+using MiniWebDeploy.Deployer.Features.Installation.PreInstallation;
+using Moq;
+using NUnit.Framework;
+
+namespace MiniWebDeploy.Deployer.Tests.Features.Installation
+{
+    [TestFixture]
+    public class PreInstallationTaskListTests
+    {
+        [Test]
+        public void BeforeInstallationIsCalledForAllTasks()
+        {
+            var task = new Mock<IPreInstallationTask>();
+
+            var taskList = new PreInstallationTaskList();
+            taskList.Add(task.Object);
+            taskList.Add(task.Object);
+
+            taskList.PerformTasks(new InstallationConfiguration(null, null));
+
+            task.Verify(x => x.BeforeInstallation(It.IsAny<InstallationConfiguration>()), Times.Exactly(2));
+        }
+    }
+}
